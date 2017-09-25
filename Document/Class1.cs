8502fa1148c64d087c2d0bfcf8fa667bcf6c8b51
fjb43008@ly.com <fjb43008@ly.com>
2017-09-25 11:50:50 +0800

    111

--- Document/Class1.cs
+++ Document/Class1.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Document
 {
     public class Class1
     {
+
+
+        public int hehe(int i)
+        {
+            var list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            list.Where(v => v.Equals("2"));
+            Console.WriteLine(i);
+            return i;
+        }
     }
 }
