dd2ea1265198a8a4544cffa694fa1c8f34130908
fjb43008@ly.com <fjb43008@ly.com>
2017-09-26 11:18:38 +0800

    2121211111

--- build.bat
+++ build.bat
@@ -1,14 +1,10 @@
 @echo Off
-set config=%1
-if "%config%" == "" (
-   set config=Release
-)
 
 set version=3.0.0
 
 REM Build
-"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MygetCITest.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MygetCITest.sln /p:Configuration=Release
 
 REM Package
 mkdir Build
-call %nuget% pack "MygetCITest\Document\Document.csproj" -symbols -o Build -p Configuration=%config% %version%
+call %nuget% pack "MygetCITest\Document\Document.csproj" -symbols -o Build -p Configuration=Release %version%
