0c6e3ae5aa80056519d16d43c8e3bdeed45e4c78
fjb43008@ly.com <fjb43008@ly.com>
2017-09-26 12:11:07 +0800

    121211212

--- build.bat
+++ build.bat
@@ -5,6 +5,8 @@ set version=3.0.0
 REM Build
  %nuget% restore
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MygetCITest.sln /p:Configuration=Release /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MygetCITest.sln /p:Configuration=Debug /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
 
 REM Package
 mkdir Build
