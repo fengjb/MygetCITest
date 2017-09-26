828f542dbe14c5fdb0c7691e2749f1634b5f1228
fjb43008@ly.com <fjb43008@ly.com>
2017-09-26 17:03:20 +0800

    212121

--- build.bat
+++ build.bat
@@ -2,8 +2,10 @@
 
 set version=3.0.0
 
+REM Package restore
+call %NuGet% restore GoogleAnalyticsTracker\packages.config -OutputDirectory %cd%\packages -NonInteractive
+
 REM Build
- %nuget% restore
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MygetCITest.sln /p:Configuration=Release /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MygetCITest.sln /p:Configuration=Debug /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
